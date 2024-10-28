1c7939436196fcab730a6ec6f24efce0ea7b80f2
jsanchezr6 <jsanchezr6@est.ups.edu.ec>
2024-10-28 12:15:49 -0500

    first commit

---
+++ src/Recursividad.java
@@ -0,0 +1,13 @@
+public class Recursividad {
+    public int  factorial (int n){
+        System.out.println("Calculando el factorial de:  " + n );
+        //Caso base: n sea 0! y 1! son iguales a 1
+        if(n == 0 || n == 1){
+            System.out.println("Caso base alcanzado el factorial de " + n + " es 1 ");
+            return 1;
+        } 
+        int resultado =  n * factorial( n - 1);
+        System.out.println("Resultado parcial de  " + n + " factorial ("+ (n-1) +")  = " + resultado );
+        return resultado;
+    }
+}
